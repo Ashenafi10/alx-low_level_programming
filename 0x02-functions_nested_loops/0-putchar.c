68380df8cca0b5da2d2679338c517231c814c721
ashenafi10 <ashenafimeteku2020@gmail.com>
2022-04-12 14:23:11 -0700

    advance nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,25 @@
+/*
+ * File: 0-putchar.c
+ *
+ */
+
 #include "main.h"
+
 /**
-*main-main block
-*description:print -putchar in a new line
-*return:the program ruturn zero 
-*/
-int  main(void)
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-_putchar(‘_’);
-_putchar (‘p’);
-_putchar (‘u’);
-_putchar (’t);
-_putchar (‘c’);
-_putchar (‘h’);
-_putchar (‘a’);
-_putchar (‘r’);
-_putchar (\n’);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
