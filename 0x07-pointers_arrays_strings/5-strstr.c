00cde66213c5f5e48d9eef23d0bfbc9cfea0ae6d
ashenafi10 <ashenafimeteku2020@gmail.com>
2022-04-25 10:19:28 -0700

    this is task 0x07-pointers_arrays_string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
 /**
+
  * _strstr - Entry point
+
  * @haystack: input
+
  * @needle: input
+
  * Return: Always 0 (Success)
- */
+
+*/
 
 char *_strstr(char *haystack, char *needle)
 	  
